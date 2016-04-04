06597bde547ea240d16daecebf880a968f6515b8
DogBlaster <l.gouthiere@online.fr>
2016-04-04 18:41:08 +0200

    Added files via upload

---
+++ membres-equipe.php
@@ -0,0 +1,39 @@
+﻿<!DOCTYPE html>
+<!-- Projet DogAnima            
+     embres-equipe.php                 
+     MAJ du 13/11/2015       
+	 MAJ du 12/01/2015       
+     Rem : Tout navigateur      
+     Auteur : Gouthière Laurent 
+-->
+<html>
+<head>
+<meta charset="utf-8" lang="fr" />
+<meta name="viewport" content="width=device-width" />
+<!--[if lt IE 9]>
+<script src="http://github.com/aFarkas/html5shiv/blob/master/dist/html5shiv.js"></script>
+<![endif]-->
+<meta name="keywords" content="DogAnima,Indre et Loire,tours,garde de chien,petsitting,famille d&#39;accueil,pension,chien"/>
+<meta name="description" content="Garde de chien en accueil ou à domicile en Indre et Loire DogAnima"/>
+<meta name="robots" content="noindex,nofollow" />
+<title>DogAnima - Mentions légales</title>
+<link rel="icon" type="img/x-ico" href="img/doganima.ico" />
+<link href="css/menu.css" type="text/css" rel="stylesheet" media="screen" />
+<link href="css/styles.css" type="text/css" rel="stylesheet" media="screen" />
+</head>
+<body>
+<?php include ("header.inc.php"); ?>
+<p></p>
+<h1>Membres de l'équipe</h1>
+<div class="c9">
+<div class="justify">
+Emmanuel LEBLANC et Laurent GOUTHIERE: Graphistes développeurs
+<br /><br />
+</div>
+</div>
+<br /><br /> 
+</section>	
+<?php include("footer.inc.php"); ?>
+</body>
+</html>
+ 
