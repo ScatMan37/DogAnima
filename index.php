7aa89319a3a0c71235aaf79695478a50eb171f71
DogBlaster <l.gouthiere@online.fr>
2016-04-04 15:16:34 +0200

    Added files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+﻿<!-- saved from url=(0022)http://internet.e-mail -->
+<!DOCTYPE html>
+<!-- Projet DogAnima            
+     index.html                 
+     Création 13/11/2015       
+	 MAJ du 24/03/2016           
+     Auteur : Gouthière Laurent 
+	 Rem : Tout navigateur et plus de tables  
+-->
+<html>
+<head>
+<meta charset="utf-8" lang="fr" />
+<meta name="viewport" content="width=device-width" />
+<!--[if lt IE 9]>
+<script src="http://github.com/aFarkas/html5shiv/blob/master/dist/html5shiv.js"></script>
+-->
+<meta name="keywords" content="DogAnima,Indre et Loire,tours,garde de chien,petsitting,famille d&#39;accueil,pension,chien"/>
+<meta name="description" content="Garde de chien en accueil ou à domicile en Indre et Loire DogAnima"/>
+<meta name="robots" content="index,follow" />
+<title>DogAnima - Garde de chiens à domicile ou en accueil</title>
+<link rel="icon" type="img/x-ico" href="img/doganima.ico" />
+<link href="css/menu.css" type="text/css" rel="stylesheet" media="screen" />
+<link href="css/styles.css" type="text/css" rel="stylesheet" media="screen" />
+</head>
+<body>
+<?php include ("headerIndex.inc.php"); ?>
+<!-- h1 en dehors de section -->
+<h1 class="title"><strong><em>Nos solutions pour la garde de chiens</em></strong></h1>
+<section>
+<p></p>
+<div class="index1">
+Nous connaissons bien nos animaux de compagnie chiens. Ils ont tous un caractère bien marqué et c'est cela qui fait leur personnalité. 
+Notre association <a class="alink" href="contactcm.php"  title="Situer ou contacter DogAnima">DogAnima</a> propose différentes formules de gardiennage. Ceux 
+sont des véritables solutions alternatives à la pension pour chien.
+</div>
+<p></p>
+<div class="index21">
+<b>La garde en famille d’accueil</b>
+<p></p>
+Cette prestation est la plus demand&eacute;e pour faire garder son chien. Le jour de votre d&eacute;part en vacances, vous confiez votre compagnon à l’une 
+de nos <a class="alink" href="tarifs.php"  title="Pet Sitter familles d'accueil">familles d’accueil</a> qui saura lui apporter toute l’attention dont il aura besoin.
+La garde de chien en famille d’accueil est id&eacute;ale pour les chiens qui n’aiment pas la solitude et qui ont besoin d’attention.
+<p></p>
+<b>La garde à domicile</b>
+<p></p>
+Pour cette garde, votre chien ne se d&eacute;place pas car c’est la nounou qui prend place à votre domicile pour s’occuper de votre compagnon. Le 
+temps de votre absence, le <a class="alink" href="tarifs.php"  title="Intervenants Pet Sitter">Pet Sitter</a> s’installe chez vous pour garder votre chien avec l’obligation de dormir à votre domicile.
+</div>
+
+<div class="index22">
+Pendant la journée le Pet Sitter est libre de ses occupations (travail, sport ou autre) Le soir au lieu de retourner à son domicile, il se rend 
+chez vous pour retrouver votre compagnon et s’occuper de lui jusqu’à votre retour.
+<p></p>
+La garde à domicile n'est pas du homesitting : les tâches m&eacute;nag&egrave;res ou autres activit&eacute;s du quotidien ne font pas partie des missions 
+du Pet Sitter. La principale mission du Pet Sitter est de s’occuper du bien-être de votre animal jusqu’à votre retour : nourriture, eau, promenades, câlins, etc.
+<p></p>
+La garde de chien à domicile est id&eacute;ale pour les chiens qui n’aiment pas le changement d’environnement et qui sont habitu&eacute;s à leur confort.
+</div>
+<br />
+<div class="center">
+<img class="round" style="width: 100%;" src="img/dogan_2_x.jpg" height="190" width="945" alt="DogAnima" title="DogAnima" />
+</div>
+</section>
+<?php include("footer.inc.php"); ?>
+<script src="js/index.js" type="text/javascript" charset="utf-8"></script>
+</body>
+</html>
