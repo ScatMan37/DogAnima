c196271308ac9c1844841a95d218258596fdb7f6
DogBlaster <l.gouthiere@online.fr>
2016-04-04 18:47:12 +0200

    Update footer.inc.php
    Ajout du lien membre de l'équipe.

--- footer.inc.php
+++ footer.inc.php
@@ -3,7 +3,7 @@
 <br />
 <a class="foot3" href="credits.php" title="Crédits">Crédits</a>
 <a class="foot3" href="mentions-legales.php" title="Mentions légales">Mentions légales</a>
-<a class="foot3" href="" title="Se connecter" target="_blank">Se connecter</a>
+<a class="foot3" href="membres-equipe.php" title="Membres-equipe" target="_blank">Membres de l'équipe</a>
 <a class="foot3" href="http://www.netpresta.fr" title="NetPresta Philippe Bouquet" target="_blank">NetPresta</a>
 <br />
 <p>
